d16659eb3dc9a311f6953aa02705b46ad80e1065
Max <49609951+MadMax240484@users.noreply.github.com>
2022-03-20 21:44:41 +0300

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -377,6 +377,7 @@ else
     set -x
     exec sg vpn -c "openvpn --cd $dir --config $conf $ext_args \
                ${OTHER_ARGS:-} ${MSS:+--fragment $MSS --mssfix}"
-    exec sg iptables -c "ip r | grep tun0| awk '/tun0 proto kernel*/ {print $1}' | xargs -0 \
-    iptables -t nat -A POSTROUTING -s $net -o eth0 -j MASQUERADE"                                  
+    sleep 10;
+    ip r | grep tun0| awk '/tun0 proto kernel*/ {print $1}' | xargs -0 \
+    iptables -t nat -A POSTROUTING -s $net -o eth0 -j MASQUERADE                               
 fi
